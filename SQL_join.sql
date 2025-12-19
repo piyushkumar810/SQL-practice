f6ea51c7b6727547f632ef183bf7676ba0449672
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-12-19 22:58:36 +0530

    chnages

---
+++ SQL_join.sql
@@ -0,0 +1,69 @@
+
+-- creating employee table
+create table employees3(
+    employee_id serial primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    department_id integer
+);
+
+select * from employees3;
+
+insert into employees3(first_name, last_name, department_id)values
+('rahul', 'sharma', 101),
+('priya', 'metha',102),
+('Ankit', 'verma',103),
+('simran', 'kaur', NULL),
+('aman', 'singh',101);
+
+
+-- creating department table
+
+drop table if EXISTS department3;
+
+create table department3(
+    department_id int primary key,
+    department_name varchar(50)
+);
+
+insert into department3(department_id, department_name)values
+(101,'sales'),
+(102, 'marketing'),
+(103,'it'),
+(104, 'HR');
+
+select * from department3;
+
+
+-- performing inner join retrieve a matching value from both of the table 
+select e.employee_id,e.first_name,e.last_name,
+       d.department_name
+from
+employees3 e
+INNER JOIN
+department3 d
+ON e.department_id=d.department_id;
+
+-- perform left join retrieve employee3 and their department name, include those without a department
+select e.employee_id,e.first_name,e.last_name,
+       d.department_name
+FROM employees3 e LEFT JOIN department3 d
+on e.department_id=d.department_id;
+
+-- performing right join
+select e.employee_id,e.first_name,e.last_name,
+       d.department_id,d.department_name
+FROM employees3 e 
+RIGHT JOIN department3 d
+on e.department_id=d.department_id;
+
+
+-- performing fulll outer join
+-- select e.employee_id,e.first_name,e.last_name,
+--        d.department_id,d.department_name
+-- FROM employees3 e 
+-- FULL OUTER JOIN department3 d
+-- on e.department_id=d.department_id;
+
+-- not supportd in sql
+
